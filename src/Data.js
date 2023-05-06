a8ebc2d2a510e46ba62ad76996246867d7df79db
Prateek1013 <prateek9771196112@gmail.com>
2023-05-07 03:17:36 +0530

    modified Data.js

--- src/Data.js
+++ src/Data.js
@@ -7,38 +7,38 @@ module.exports=[
             price:350,
             url:"https://imgd.aeplcdn.com/664x374/n/cw/ec/121943/verna-facelift-exterior-right-front-three-quarter.jpeg?isig=0&q=75",
             seats:4,
-            color:"grey",
+            color:"black",
             plate:"AC2365",
             engine:"ICI petrol engine",
-            stuff:"kuch v"
+            description:"kuch v"
         }
     },
     {
-        id:1,
+        id:2,
         book_status:false,
         details:{
-            name:"Hyundai Verna",
-            price:350,
-            url:"https://imgd.aeplcdn.com/664x374/n/cw/ec/121943/verna-facelift-exterior-right-front-three-quarter.jpeg?isig=0&q=75",
+            name:"Mercedes Benz",
+            price:3350,
+            url:"https://media.zigcdn.com/media/model/2021/May/gla-1_360x240.jpg",
             seats:4,
-            color:"grey",
+            color:"white",
             plate:"AC2365",
             engine:"ICI petrol engine",
-            stuff:"kuch v"
+            description:"kuch v"
         }
     },
     {
-        id:1,
+        id:3,
         book_status:false,
         details:{
-            name:"Hyundai Verna",
-            price:350,
-            url:"https://imgd.aeplcdn.com/664x374/n/cw/ec/121943/verna-facelift-exterior-right-front-three-quarter.jpeg?isig=0&q=75",
+            name:"Ferrari",
+            price:3650,
+            url:"https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/PortofinoModelImage.jpg",
             seats:4,
-            color:'grey',
+            color:"red",
             plate:"AC2365",
             engine:"ICI petrol engine",
-            stuff:"kuch v"
+            description:"kuch v"
         }
     }
 ];
